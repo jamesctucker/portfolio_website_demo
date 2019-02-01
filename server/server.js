66f6fcaecdbbfd63d89718bfc4144471cf6e8194
James Tucker <jamesctucker@protonmail.com>
2019-02-01 16:25:55 -0600

    set up pool.js file and started setting up server.js file

--- server/server.js
+++ server/server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
+const router = require('./routes/portfolio_router.js');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /** ---------- ROUTES ---------- **/
-
+app.use('/portfolio', router);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
