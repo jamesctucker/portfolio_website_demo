0c04fc96c5475bb4837e96a290aa32379ddf966e
James Tucker <jamesctucker@protonmail.com>
2019-02-02 19:06:01 -0600

    this creates the admin page; not yet complete

--- database.sql
+++ database.sql
@@ -20,11 +20,11 @@ VALUES ('React'), ('jQuery'), ('Node'), ('SQL'), ('Redux'), ('HTML');
 -- insert project data
 INSERT INTO "projects"("id", "name", "description", "thumbnail", "website", "github", "date_completed", "tag_id") 
 VALUES(1, 'Feedback App', 'This is a multi-step form that allows users to submit feedback. 
-User-submitted ratings are then stored in a database, and view via an admin page.', 'public/images/FeedbackThumbnail.png', 
+User-submitted ratings are then stored in a database, and viewed via an admin page.', './../images/FeedbackThumbnail.png', 
 'https://floating-depths-51536.herokuapp.com/#/', 'https://github.com/jamesctucker/weekend-challenge-5-feedback', '2019-01-23', 1;
 
 INSERT INTO "projects"("id", "name", "description", "thumbnail", "website", "github", "date_completed", "tag_id") 
 VALUES(2, 'Giphy Search App', 'This app was part of a group project. It enables users to search for specific gifs via tbe GIPHY api. 
 Results are then rendered on the user interface, and users can then favorite whichever ones they like. Favorited gifs are displayed 
-in a separate ''page'' where the user can categorize each favorited gif. All of this data is stored in a database.', 'public/images/GiphyThumbnail.png', 
+in a separate ''page'' where the user can categorize each favorited gif. All of this data is stored in a database.', './../images/GiphyThumbnail.png', 
 'https://fierce-bayou-69537.herokuapp.com/', 'https://github.com/adunahee/giffers-giphy-group-project', '2019-02-01', 5;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import { HashRouter as Router, Link, Route } from 'react-router-dom';
 import './App.css';
 import PortfolioPage from './../Portfolio/PortfolioPage';
 import Drawer from './../Portfolio/Drawer';
+import Admin from './../Admin/Admin';
 
 class App extends Component {
   // Renders the entire app on the DOM
@@ -18,6 +19,7 @@ class App extends Component {
               </div>
             </header>
             <Route exact path='/' component={PortfolioPage} />
+            <Route exact path='/admin' component={Admin} />
           </div>
         </Router>
       </div>
